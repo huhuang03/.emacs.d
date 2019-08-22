fbf8740197e1a0b0f9e5dec42d25dc444981f143
huhuang03 <huhuang03@gmail.com>
2019-08-22 08:56:33 +0800

    add th_folder-to-src

--- init.el
+++ init.el
@@ -28,13 +28,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(doc-view-ghostscript-options
-   '("-dNOSAFER" "-dNOPAUSE" "-sDEVICE=png16m" "-dTextAlphaBits=4" "-dBATCH" "-dGraphicsAlphaBits=4" "-dQUIET"))
+   (quote
+    ("-dNOSAFER" "-dNOPAUSE" "-sDEVICE=png16m" "-dTextAlphaBits=4" "-dBATCH" "-dGraphicsAlphaBits=4" "-dQUIET")))
  '(doc-view-ghostscript-program "/usr/local/bin/gs")
- '(org-agenda-files '("~/source/org/gtd.org"))
+ '(org-agenda-files (quote ("~/source/org/gtd.org")))
  '(package-selected-packages
-   '(epa-file elfeed jedi flycheck htmlize ox-reveal counsel ace-window org-bullets try use-package magit yaml-mode evil monokai-theme key-chord hungry-delete which-key general groovy-mode elpy smali-mode exec-path-from-shell markdown-mode helm-swoop csharp-mode omnisharp evil-surround auto-complete yasnippet auto-complete-c-headers projectile all-the-icons lua-mode))
+   (quote
+    (epa-file elfeed jedi flycheck htmlize ox-reveal counsel ace-window org-bullets try use-package magit yaml-mode evil monokai-theme key-chord hungry-delete which-key general groovy-mode elpy smali-mode exec-path-from-shell markdown-mode helm-swoop csharp-mode omnisharp evil-surround auto-complete yasnippet auto-complete-c-headers projectile all-the-icons lua-mode)))
  '(safe-local-variable-values
-   '((flycheck-clang-include-path quote
+   (quote
+    ((flycheck-clang-include-path quote
                                   ("/Users/th/source/cppPacman/include/"))
      (flycheck-clahg-include-path quote
                                   ("/Users/th/source/cppPacman/include/"))
@@ -46,7 +49,8 @@
      (flycheck-clang-includes quote
                               ("~/source/cppPacman/include"))
      (flycheck-clang-includes
-      '("~/source/cppPacman/include")))))
+      (quote
+       ("~/source/cppPacman/include")))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
