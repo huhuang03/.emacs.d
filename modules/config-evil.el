46222e44951a7a63e3803d606605f241aba3dad8
th <huhuang0@gmail.com>
2022-03-14 01:51:12 +0800

    add fc to config config file. although it's not good now

--- modules/config-evil.el
+++ modules/config-evil.el
@@ -1,10 +1,17 @@
-;; put it for now, but better is separate to somewhere else?
+;; this need some better define
+(defun go-config-home ()
+    (interactive)
+    (find-file (expand-file-name "modules/" user-emacs-directory)))
+
 (use-package evil
     :straight t
     :init
         (evil-mode 1)
     :bind
+        ;; put it for now, but better is separate to somewhere else?
         (:map evil-normal-state-map
-            ("<SPC> wm" . toggle-frame-fullscreen)))
+            ("<SPC> wm" . toggle-frame-fullscreen)
+            ("<SPC> fc" . go-config-home)
+            ))
 
 (provide 'config-evil)
