9f9e79210ec750b9a7e0d80a576b576787b878fd
th <huhuang0@gmail.com>
2022-03-14 08:46:54 +0800

    set face height

--- modules/config-ui.el
+++ modules/config-ui.el
@@ -2,5 +2,10 @@
 (use-package doom-themes
     :straight t
     :config
-        (load-theme 'doom-one t))
+    (load-theme 'doom-one t))
+
+
+;; set font size, default is 100
+(set-face-attribute 'default nil :height 120)
+
 (provide 'config-ui)
