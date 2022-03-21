dad586a07177dd8ec16fd14fd56c4f845d8899c9
th <huhuang0@gmail.com>
2022-03-22 06:29:12 +0800

    add lsp-mode

--- modules/config-depend-local.el
+++ modules/config-depend-local.el
@@ -1,4 +1,7 @@
 (setq initial-buffer-choice (expand-file-name "org/gtd.org" SOURCE_HOME))
 
+(add-to-list 'load-path (expand-file-name "th-posts.emacs" SOURCE_HOME))
+
+(require 'th-posts-mode)
 
 (provide 'config-depend-local)
