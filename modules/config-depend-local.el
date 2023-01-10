5c557054192278c0a1e37c98ff266a99d717dcdc
胡文凡 <wenfan.hu@infaith.com.cn>
2023-01-10 09:52:01 +0800

    feat: smart initial_folder

--- modules/config-depend-local.el
+++ modules/config-depend-local.el
@@ -1,4 +1,8 @@
-(setq initial-buffer-choice (expand-file-name "org/gtd.org" SOURCE_HOME))
+(setq initial-buffer-choice
+      (if (file-exists-p (expand-file-name "org/gtd_local.org" SOURCE_HOME))
+	  (expand-file-name "org/gtd_local.org" SOURCE_HOME)
+	  (expand-file-name "org/gtd.org" SOURCE_HOME)
+	))
 
 (add-to-list 'load-path (expand-file-name "th-posts.emacs" SOURCE_HOME))
 
