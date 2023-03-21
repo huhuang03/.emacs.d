a87f58b6a09836fce47c29eb5239f7252bcf6618
胡文凡 <wenfan.hu@infaith.com.cn>
2023-03-21 18:08:28 +0800

    up

--- modules/config-languages.el
+++ modules/config-languages.el
@@ -6,4 +6,13 @@
   ;; (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode)))
   (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode)))
 
+;; yaml
+(use-package yaml-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist
+		'("\\.yml\\'" . yaml-mode)
+		'("\\.yaml\\'" . yaml-mode)
+	       ))
+
 (provide 'config-languages)
