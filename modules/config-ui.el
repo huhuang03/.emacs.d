5e57c4370d0298718e7044bbdeab06a770df4681
胡文凡 <wenfan.hu@infaith.com.cn>
2023-07-04 18:12:21 +0800

    up

--- modules/config-ui.el
+++ modules/config-ui.el
@@ -4,6 +4,23 @@
     :config
     (load-theme 'doom-one t))
 
+;; frist try set font to intel mono
+;; print all mono font
+(defun th-print-mono-font-family ()
+  (interactive)
+  (message
+   "%s"
+   (seq-filter (lambda (font)
+		(when-let ((info (font-info font)))
+		(string-match-p "spacing=100" (aref info 1))))
+		(font-family-list))))
+
+;; set font to IntelOne Mono
+
+
+
+(font-family-list)
+
 
 ;; set font size, default is 100
 (set-face-attribute 'default nil :height 120)
