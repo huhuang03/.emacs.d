dd1df34c59a12bf2f7f02d7cc7510228927e4e3e
huhuang03 <huhuang03@gmail.com>
2023-09-10 20:22:34 +0800

    up

--- modules/config-languages.el
+++ modules/config-languages.el
@@ -1,11 +1,3 @@
-;; csharp
-
-(use-package csharp-mode
-  :ensure t
-  :config
-  ;; (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode)))
-  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode)))
-
 (use-package tex
   :ensure auctex)
 ;; (use-package auctex
