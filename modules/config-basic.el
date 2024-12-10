3fa2c6376dd9e5a9ac26f83441a3595f1a1bbb9b
胡文凡 <wenfan.hu@infaith.com.cn>
2024-12-10 16:28:55 +0800

    up

--- modules/config-basic.el
+++ modules/config-basic.el
@@ -2,4 +2,5 @@
 (setq straight-use-package-by-default t)
 
 (use-package org)
+(setq-default tab-width 4)
 (provide 'config-basic)
--- modules/config-languages.el
+++ modules/config-languages.el
@@ -1,3 +1,12 @@
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
 (use-package yaml-mode
   :ensure t
   :config
